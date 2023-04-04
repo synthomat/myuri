ff226e7e14e13cac591b341428d38dd058cfb38c
Anton Zering <synth@lostprofile.de>
2023-04-05 01:26:06 +0200

    re-organized settings handler and view

--- src/myuri/web/handler.clj
+++ src/myuri/web/handler.clj
@@ -1,15 +1,11 @@
 (ns myuri.web.handler
   (:require [bidi.ring :refer [make-handler]]
-            [cheshire.core :as json]
-            [myuri.model :as m]
             [myuri.web.auth.handler :as ah]
+            [myuri.web.settings.handler :as sh]
             [myuri.web.bookmarks.handler :as bh]
             [myuri.web.middleware :as mw]
-            [myuri.web.utils :refer [user-id]]
             [myuri.web.views :as v]
-
-            [ring.util.response :as resp])
-  (:import (java.time.format DateTimeFormatter)))
+            [ring.util.response :as resp]))
 
 ;; Utils ----------------------------------------------------------------------
 
@@ -22,78 +18,26 @@
       (resp/status 404)))
 
 
-;; Backups Handlers -----------------------------------------------------------
-(defn format-date
-  "docstring"
-  [format date]
-  (-> format
-      DateTimeFormatter/ofPattern
-      (.format date)))
-
-(defn backup-endpoint
-  "docstring"
-  [req]
-  (v/backup-view req))
-
-(defn send-json-file
-  "Makes the browser download the provided file"
-  [data file-name]
-  (-> (resp/response data)
-      (resp/header "Content-Disposition" (format "attachment; filename=\"%s\"" file-name))
-      (resp/content-type "application/json")))
-
-(defn export-handler
-  "docstring"
-  [{:keys [ds] :as req}]
-
-  (let [export-data (m/export-bookmarks ds (user-id req))
-        ts (:time export-data)
-        file-name (format "myuri-export_%s.json" (format-date "yyMMddHHmm" ts))
-        json-data (-> export-data
-                      (assoc :time (format-date "yyyy-MM-dd HH:mm:ss" ts))
-                      (json/encode {:pretty true}))]
-    (send-json-file json-data file-name)))
-
-(defn token-settings-handler
-  "docstring"
-  [req]
-  (v/token-view req))
-
-(defn ui-settings-handler
-  "docstring"
-  [{:keys [ds] :as req}]
-  (let [us (m/get-user-setting ds (user-id req) nil)
-        setting-map (->> (map (fn [s] [(keyword (:user_settings/setting_name s)) (:user_settings/json_value s)]) us)
-                         (into {}))]
-    (v/ui-settings-view req setting-map)))
-
-(defn config-toggle-handler
-  "docstring"
-  [{:keys [ds] :as req}]
-
-  (println (m/update-user-setting ds (user-id req) (-> req :params)))
-  (-> (resp/response {:value "ok"})))
-
 ;; Routes and Middlewares -----------------------------------------------------
 (def web-routes
   ["/" {""                 bh/index-handler
         "new"              bh/new-bookmark-handler
         ["bookmarks/" :id] {:delete {"" bh/delete-bookmark-handler}
                             "/edit" bh/edit-bookmark-handler}
-        "backup"           {""    backup-endpoint
-                            :post {"/export" export-handler}}
+        "backup"           {""    sh/backup-endpoint
+                            :post {"/export" sh/export-handler}}
         "auth/"            {"login"    ah/login-handler
                             :post      {"logout" ah/logout}
                             "register" ah/register-handler}
         "settings"         {""        (fn [req] (resp/redirect "/settings/ui"))
-                            "/tokens" token-settings-handler
-                            "/ui"     ui-settings-handler
-                            "/backup" backup-endpoint}
+                            "/tokens" sh/token-settings-handler
+                            "/ui"     sh/ui-settings-handler
+                            "/backup" sh/backup-endpoint}
         ;; API ----------------------------------------------------------------
         "api/"             {"bookmarks"       {"" (fn [req]
                                                     (resp/response
                                                       {:response "ok"}))}
-                            ["user/settings"] {:put {"" config-toggle-handler}}}
+                            ["user/settings"] {:put {"" sh/config-toggle-handler}}}
 
         true               not-found-handler}])
 
--- src/myuri/web/settings/views.clj
+++ src/myuri/web/settings/views.clj
@@ -1,1 +1,105 @@
-(ns myuri.web.settings.views)
+(ns myuri.web.settings.views
+  (:require [myuri.web.views :as v]
+            [ring.util.anti-forgery :refer [anti-forgery-field]]))
+
+(defn uri-prefix
+  "docstring"
+  [req sub]
+  (clojure.string/starts-with? (:uri req) sub))
+
+(defn active
+  "docstring"
+  [req prefix]
+  (and (uri-prefix req prefix) "is-active"))
+
+(defn settings-nav
+  "docstring"
+  [req]
+  (let [is-active (partial active req)]
+    [:aside.menu
+     [:p.menu-label "General"]
+     [:ul.menu-list
+      [:li [:a {:href "/settings/ui" :class (is-active "/settings/ui")} "User Interface"]]
+      [:li [:a {:href "/settings/backup" :class (is-active "/settings/backup")} "Backup"]]
+      [:li [:a {:href "/settings/import" :class (is-active "/settings/import")} "Import"]]]
+
+     #_#_[:p.menu-label "Integrations"]
+             [:ul.menu-list
+              [:li [:a {:href "/settings/tokens" :class (is-active "/settings/tokens")} "Tokens"]]]]))
+
+(defn settings-layout
+  "docstring"
+  [req & children]
+  (v/layout req
+          [:div.container {:style "margin-top: 30px"}
+           [:h3.is-size-3 {:style "margin-bottom: 20px"} "Account Settings"]
+           [:div.columns
+            [:div.column.is-2 (settings-nav req)]
+
+            [:div.column children]]]))
+
+(defn backup-view
+  [req]
+  (settings-layout req
+                   [:div
+
+                    [:div
+                     [:h3.title.is-3 "Backup"]
+                     [:form {:action "/backup/export" :method "post"}
+                      (anti-forgery-field)
+                      [:input {:type "submit" :value "Download Export"}]]]
+
+                    #_[:div {:style "margin-top: 50px"}
+                       [:h2 "Restore"]
+                       [:form {:action "/backup/import" :method "post"}
+                        (anti-forgery-field)
+                        [:input {:type "file" :name "data"}]
+                        [:input {:type "submit" :value "Import"}]]]]))
+
+
+(defn token-view
+  "docstring"
+  [req]
+
+  (settings-layout
+    req
+    [:h3.is-size-3 "Tokens"] [:button.button.is-primary "Create Token"]
+
+    [:table.table.is-fullwidth.is-hoverable
+     [:thead
+      [:tr
+       [:th "Name"]
+       [:th "Id"]
+       [:th "Token"]
+       [:th "Valid until"]
+       [:th ""]]]
+     [:tbody
+      [:tr
+       [:td "iOS App"]
+       [:td "98e9a"]
+       [:td "gKWUrdDNNNrdy3psURELxSdb2NprCtIUxd97e5sC"]
+       [:td ""]
+       [:td [:a {:href "#"} "delete"]]]]]))
+
+(defn ui-settings-view
+  "docstring"
+  [req smap]
+
+  (settings-layout
+    req
+    [:h3.title.is-3 "UI Config"]
+
+    [:form.config-toggles
+     #_[:div.field
+        [:div.control
+         [:label.checkbox
+          [:input {:type "checkbox" :name "detail_fetching" :checked (:detail_fetching smap)}] " Details fetching"]]
+        [:p.help "Given only the site url, it will fetch site details in the background"]]
+
+     [:div.field
+      [:div.control
+       [:label.checkbox
+        [:input {:type "checkbox" :name "display_icons" :checked (:display_icons smap)}] " Display site icons"]]
+      [:p.help "Will load site icons from original location"]]]))
+
+
